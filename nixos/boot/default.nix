207228e80639110f7fbf644dfa1fb7fd1678096d
AllenCaine <56565659+AllenCaine@users.noreply.github.com>
2025-12-22 13:47:46 +0100

    boot changed for uefi

--- nixos/boot/default.nix
+++ nixos/boot/default.nix
@@ -1,11 +1,12 @@
 # Boot loader configuration module
+# Supports both BIOS and UEFI, with BIOS Boot Partition for GPT disks
 { config, lib, pkgs, ... }:
 {
    
     boot.loader.grub.enable = true;
-    boot.loader.grub.device = "/dev/sda";
+    boot.loader.grub.efiSupport = false;
     boot.loader.grub.useOSProber = true;
-    boot.loader.timeout = 0;
+    boot.loader.timeout = 5;
     boot.loader.grub.timeoutStyle = "menu";
   
 }
