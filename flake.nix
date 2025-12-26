8bec1f8f3716ee78dfdc03e72479a0752ae05437
ArcticOwl-Dev <56565659+ArcticOwl-Dev@users.noreply.github.com>
2025-12-26 16:18:44 +0100

    simplified flake.nix and changed snowfire config

--- flake.nix
+++ flake.nix
@@ -34,34 +34,14 @@
     ...
   } @ inputs: let
     # Supported systems for your flake packages, shell, etc.
-    systems = [
-      "aarch64-linux"
-      "i686-linux"
-      "x86_64-linux"
-      "aarch64-darwin"
-      "x86_64-darwin"
-    ];
-    # This is a function that generates an attribute by calling a function you
-    # pass to it, with each system as an argument
-    forAllSystems = nixpkgs.lib.genAttrs systems;
+    system = "x86_64-linux";
+
   in {
-    # Your custom packages
-    # Accessible through 'nix build', 'nix shell', etc
-    packages = forAllSystems (system: import ./pkgs nixpkgs.legacyPackages.${system});
-    # Formatter for your nix files, available through 'nix fmt'
-    # Other options beside 'alejandra' include 'nixpkgs-fmt'
-    formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.alejandra);
+    packages = import ./pkgs nixpkgs.legacyPackages.${system};
+    formatter = nixpkgs.legacyPackages.${system}.nixpkgs-fmt;
 
-    # Your custom packages and modifications, exported as overlays
     overlays = import ./overlays {inherit inputs;};
-    # Reusable nixos modules you might want to export
-    # These are usually stuff you would upstream into nixpkgs
-    # nixosModules = import ./modules/nixos;
-    # Reusable home-manager modules you might want to export
-    # These are usually stuff you would upstream into home-manager
-    # homeManagerModules = import ./modules/home-manager;
-
-    # NixOS configuration entrypoint
+ 
     # Available through 'nixos-rebuild --flake .#your-hostname'
     nixosConfigurations = {
       # Machine 1: snowfire
@@ -70,6 +50,25 @@
         modules = [
           ./nixos/configuration.nix
           ./hosts/snowfire/configuration.nix
+
+          # home-manager
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+
+            home-manager.users = {
+              r00t = import [
+                ./home/home.nix
+                ./host/snowfire/home.nix
+              ]
+
+            };
+            home-manager.extraSpecialArgs = {
+              inherit inputs;
+              style = import ./hosts/snowfire/style.nix
+            };
+          }
         ];
       };
       # Machine 2: stardust
@@ -79,32 +78,7 @@
           ./nixos/configuration.nix
           ./hosts/stardust/configuration.nix
         ];
-      };
-    };
-
-    # Standalone home-manager configuration entrypoint
-    # Available through 'home-manager --flake .#your-username@your-hostname'
-    homeConfigurations = {
-      "r00t@snowfire" = home-manager.lib.homeManagerConfiguration {
-        # Home-manager requires 'pkgs' instance
-        pkgs = nixpkgs.legacyPackages.x86_64-linux; 
-        extraSpecialArgs = {inherit inputs;};
-        modules = [
-          ./home/home.nix
-          ./hosts/snowfire/home.nix
-        ];
-      };
-      "r00t@stardust" = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages.x86_64-linux; 
-        extraSpecialArgs = {
-          inherit inputs;
-          style = import ./hosts/stardust/style.nix;
-        };
-        modules = [
-          ./home/home.nix
-          ./hosts/stardust/home.nix
-        ];
-      };
-    };
-  };
+      }
+    }
+  }
 }
--- hosts/snowfire/home.nix
+++ hosts/snowfire/home.nix
@@ -1,12 +1,28 @@
-# Home Manager configuration for snowfire
-{ config, lib, pkgs, inputs, ... }:
+# Home Manager configuration for stardust
+{ config, lib, pkgs, inputs, style, ... }:
 {
+  imports = [
+    ../../home/browser/brave.nix
+    ../../home/browser/firefox.nix
+
+    ../../home/cli/cli.nix
+    ../../home/cli/git.nix
+
+    ../../home/desktop/hyprland.nix
+    ../../home/desktop/wlogout.nix
+    ../../home/desktop/hyprlock.nix
+    ../../home/desktop/waybar.nix
+    
+    ../../home/appLauncher/walker.nix
+    ../../home/appLauncher/vicinae.nix
+  ];
+
   home = {
     username = "r00t";
     homeDirectory = "/home/r00t";
   };
 
-  # Machine-specific Home Manager configuration
-  # Add your snowfire-specific user settings here
+  # Machine-specific Home Manager configuration for stardust
+  # Add your stardust-specific user settings here
 }
 
